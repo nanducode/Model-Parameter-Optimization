80af91f0644ee1457e544c31b939baae5255672f
Sam Partee <Partees21@gmail.com>
2018-09-17 15:20:00 -0400

    Make shell script to compile model

---
+++ compile_model.sh
@@ -0,0 +1,23 @@
+
+#!/bin/sh
+
+
+# Compile Flexible Model System
+mkdir -p ../MOM6-examples/build/intel/shared/repro
+
+(cd ../MOM6-examples/build/intel/shared/repro/; rm -f path_names; ../../../../src/mkmf/bin/list_paths -l ../../../../src/FMS; \
+../../../../src/mkmf/bin/mkmf -t ../../../../src/mkmf/templates/sam-linux-gnu.mk \
+-p libfms.a -c '-Duse_netCDF -DSPMD' path_names)
+
+(source ../../env; make NETCDF=3 REPRO=1 libfms.a)
+
+# Compile MOM6 model
+mkdir -p ../MOM6-examples/build/intel/ocean_only/repro/
+
+(cd ../MOM6-examples/build/intel/ocean_only/repro/; rm -f path_names; \
+ ../../../../src/mkmf/bin/list_paths -l ./ ../../../../src/MOM6/{config_src/dynamic,config_src/solo_driver,src/{*,*/*}}/ ; \
+ ../../../../src/mkmf/bin/mkmf -t ../../../../src/mkmf/templates/sam-linux-gnu.mk -o '-I../../shared/repro' -p MOM6 -l '-L../../shared/repro -lfms' -c '-Duse_netCDF -DSPMD' path_names)
+
+(cd ../MOM6-examples/build/intel/ocean_only/repro/; source ../../env; make NETCDF=3 REPRO=1 MOM6)
+
+
--- run_model.py
+++ run_model.py
@@ -3,14 +3,6 @@ import shutil
 import glob
 import sys
 import subprocess
-import base64
-
-# TODO: flexible filepaths
-#       come up with dir naming system
-#       compile and run multicore
-#       iterate over multiple runs
-#       encapsulate as functions.
-#       Adjust parameters before runs in MOM_OVERRIDE
 
 run_name = "double_gyre_1"
 
@@ -23,40 +15,6 @@ remove_dir.wait()
 create_dir = subprocess.Popen("cp -r ../MOM6-examples/ocean_only/double_gyre_start ../MOM6-examples/ocean_only/" + run_name,
                               shell=True)
 create_dir.wait()
-
-
-# Compile Flexible Model System
-create_fms_build_dir = subprocess.Popen("mkdir -p ../MOM6-examples/build/intel/shared/repro",
-                                        shell=True)
-create_fms_build_dir.wait()
-setup_fms = subprocess.Popen("(rm -f path_names; ../../../../src/mkmf/bin/list_paths -l ../../../../src/FMS; \
-../../../../src/mkmf/bin/mkmf -t ../../../../src/mkmf/templates/sam-linux-gnu.mk \
--p libfms.a -c '-Duse_netCDF -DSPMD' path_names)",
-                             cwd="../MOM6-examples/build/intel/shared/repro/",
-                             shell=True)
-setup_fms.wait()
-compile_fms = subprocess.Popen("(source ../../env; make NETCDF=3 REPRO=1 libfms.a -j)",
-               cwd="../MOM6-examples/build/intel/shared/repro/",
-                               shell=True)
-compile_fms.wait()
-
-
-# Compile MOM6 model
-create_MOM_dir = subprocess.Popen("mkdir -p ../MOM6-examples/build/intel/ocean_only/repro/",
-                                  shell=True)
-create_MOM_dir.wait()
-setup_MOM = subprocess.Popen("(rm -f path_names; ../../../../src/mkmf/bin/list_paths -l \
-../ ../../../../src/MOM6/{config_src/dynamic,config_src/solo_driver,src/{*,*/*}}/ ; \
-../../../../src/mkmf/bin/mkmf -t ../../../../src/mkmf/templates/sam-linux-gnu.mk -o \
-'-I../../shared/repro' -p MOM6 -l '-L../../shared/repro -lfms' -c '-Duse_netCDF -DSPMD' path_names)",
-               cwd="../MOM6-examples/build/intel/ocean_only/repro/",
-                             shell=True)
-setup_MOM.wait()
-compile_MOM = subprocess.Popen("(source ../../env; make NETCDF=3 REPRO=1 MOM6 -j)",
-                               cwd="../MOM6-examples/build/intel/ocean_only/repro/",
-                               shell=True)
-
-
 
 # Setup restart for run
 create_restart = subprocess.Popen("mkdir -p ../MOM6-examples/ocean_only/" + run_name + "/RESTART",
